57baf2dffc9396ad7aa5107f057fd51428af33e0
MariaKrusteva <mimito_68@abv.bg>
2015-06-18 00:40:41 +0300

    Some new functionality

--- app.js
+++ app.js
@@ -1,17 +1,16 @@
-  var
-    canvas = document.getElementById("canvas"),
-    context = canvas.getContext("2d"),
-    height = 300,
-    width = 300;
+var
+  canvas = document.getElementById("canvas"),
+  context = canvas.getContext("2d"),
+  height = 300,
+  width = 300;
 
 
 function drawCanvas(){
 
-
   canvas.height = height;
   canvas.width = width;
   context.fillStyle = "black";
-  for(var i=0; i<width; i+=30){
+  _.range(0, width, 30).forEach(function(i) {
       context.beginPath();
       context.moveTo(i,0);
       context.lineTo(i,height);
@@ -21,25 +20,25 @@ function drawCanvas(){
       context.moveTo(0,i);
       context.lineTo(width, i);
       context.stroke();
-  }
+  });
 }
 
 function Point(x, y, size, ctx){
-    this.x = x;
-    this.y = y;
-    this.size = size;
-    this.ctx = ctx;
+  this.x = x;
+  this.y = y;
+  this.size = size;
+  this.ctx = ctx;
 
-    this.getX = function(){
-        return this.x;
-    }
+  this.getX = function(){
+    return this.x;
+  }
 
-    this.getY = function(){
-        return this.y;
-    }
+  this.getY = function(){
+    return this.y;
+  }
 
-    this.print = function(){
-        this.ctx.fillRect(this.x * this.size, this.y * this.size, this.size, this.size);
+  this.print = function(){
+    this.ctx.fillRect(this.x * this.size, this.y * this.size, this.size, this.size);
   }
 
 }
@@ -49,36 +48,42 @@ function getRandomInt(min, max) {
 }
 
 function getTheDirection(shipSize){
-  var x = getRandomInt(0, 10);
-  var y = getRandomInt(0 ,10);
-  var possibleDirections = [];
-  var result = {};
-  if(x+shipSize > 10){
+  var
+    x = getRandomInt(0, 10),
+    y = getRandomInt(0 ,10),
+    possibleDirections = [],
+    result = {};
+
+  if(x + shipSize > 10){
     possibleDirections.push("left")
   }
   else{
     possibleDirections.push("right")
   }
-  if(y+shipSize > 10){
+  if(y + shipSize > 10){
     possibleDirections.push("up")
   }
   else{
     possibleDirections.push("down")
   }
+
   result.directions = possibleDirections;
-  result.startPoint = new Point(x,y,30, context);
+  result.startPoint = new Point(x, y, 30, context);
+
   return result;
 }
 
 
-function drawShip(shipSize){
-  var lqlq = getTheDirection(shipSize)
-  var direction = lqlq.directions[getRandomInt(0,2)];
-  var x = lqlq.startPoint.getX();
-  var y = lqlq.startPoint.getY();
-  console.log(direction,x,y);
-  var points = [];
+function getShip(shipSize){
+  var
+    ship = getTheDirection(shipSize),
+    direction = ship.directions[getRandomInt(0,2)],
+    x = ship.startPoint.getX(),
+    y = ship.startPoint.getY(),
+    points = [];
+
   switch (direction){
+
     case "left": _.range(x, x-shipSize, -1).forEach(function(i) {
       points.push(new Point(i, y, 30, context));
     }); break;
@@ -97,14 +102,54 @@ function drawShip(shipSize){
 
   }
 
+  // points.forEach(function(point) {
+  //   point.print();
+  // })
+  // console.log(direction, points);
+  return points;
+}
+
+function checkForHit(points, enteredX, enteredY){
+  var result = false;
   points.forEach(function(point) {
-    point.print();
+    if(point.getX() == enteredX && point.getY() == enteredY){
+      point.print();
+      result = true;
+    }
   })
-  console.log(direction, points);
+  return result;
 }
 
 $(document).ready(function() {
   drawCanvas();
+  var
+    ships = _.union(getShip(5), getShip(4), getShip(4)),
+    hits = 0,
+    enteredX = null,
+    enteredY = null;
+
+    console.log(ships);
+
+  $('#checkForHitButton').on('click', function() {
+    enteredX = $('#xCoordinate').val();
+    enteredY = $('#yCoordinate').val();
+    $('#message').empty();
+    if(checkForHit(ships, enteredX, enteredY)){
+      $('#message').html('HIT');
+      hits += 1;
+    }
+    else{
+      $('#message').html('MISSED');
+    }
+    if(hits === ships.length) {
+      alert('YOU WIN!');
+    }
+  })
+
+
   //console.log(getTheDirection(5));
-  drawShip(5);
+  // drawShip(5);
+  // drawShip(4);
+  // drawShip(4);
+
 })
